abc9f80568b64155782eec1d82c652ce8a43f796
cuaubax <cuau.cortes@bax.mx>
2025-03-05 23:51:43 -0600

    chore: fixed index.ts file

--- bax-wallet/src/pages/index.tsx
+++ bax-wallet/src/pages/index.tsx
@@ -1,18 +1,5 @@
 import { Layout } from '../components/Layout';
-import { WalletBalance } from '../components/WalletBalance';
-import { TabContent } from '../components/Tabs'
 
 export default function Home() {
-  return (
-    <Layout>
-      <div className="space-y-8">
-        <div className="bg-white rounded-xl p-6 shadow-sm border border-gray-100">
-          <WalletBalance />
-        </div>
-        <div className="bg-white rounded-xl shadow-sm border border-gray-100">
-          <TabContent />
-        </div>
-      </div>
-    </Layout>
-  )
+  return <Layout />;
 }
